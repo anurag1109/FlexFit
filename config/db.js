59008371d6ecc3b79d90cafe027dcca1d631a6db
anurag1109 <bittu.anurag123@gmail.com>
2023-06-17 17:39:03 +0530

    issue fixed

--- config/db.js
+++ config/db.js
@@ -2,12 +2,12 @@ const mongoose = require("mongoose");
 // const { createClient } = require('redis');
 require("dotenv").config();
 
-const connection = mongoose.connect(process.env.MONGO);
+const connection = mongoose.connect(process.env.MONGO, { useNewUrlParser: true,useUnifiedTopology: true});
 // const client = createClient({
 //     url: process.env.REDIS
 // });
 
 module.exports = {
-    connection,
-    // client
-}
+  connection,
+  // client
+};
